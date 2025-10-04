aa8629d5928cd41953fe99dfe084442536c84383
Rijvan786 <MOHAMMADRIJVAN@outlook.com>
2025-10-04 15:29:29 +0530

    NewProject

---
+++ new.js
@@ -0,0 +1,116 @@
+  // Chat gpt question of for the practice and logic building 
+// name="rij\nva\tn"
+// j= `what is you ${name }`
+
+// console.log(j)
+
+// let r="          javascript is awesome"
+// r.toLowerCase()
+
+// k=r.toUpperCase()
+// console.log(k.trim())
+
+// let j= "This is good example"
+// k=j.replace("good","bad")
+
+// console.log(k)
+
+
+// Count how many type a
+
+// str="Learn JavaScriptaaaaaaaaaa Everyday"
+// count=0;
+// for(let i of str){
+//    if(i==="a"){
+//     count++;
+//    } 
+
+// }
+// console.log(`Count of i is ${count}`)
+
+
+
+///// Function  banai jisme format_user name se vah sentence accept
+
+
+
+// function format_user(name,age) {
+
+//     this.name=name
+//     this.age=age
+//     console.log(`My name is ${name} and  i am  ${age}  year old`)
+    
+// }
+
+// let object=new format_user("Rijvan",20)
+
+
+
+// input from the user and convett trim() and lowercase agar str ki length 10 se jyad to doble prompt do long text length aur 10 or 10< to 
+
+// perfect message print karado 
+
+// let user  = prompt("Please enter the correct no of char")
+
+// let ver=user.trim().toLowerCase()
+
+
+
+// for(let v in ver){
+//         if(v<10 && v!=10){
+//         console.log("Perfect string ")
+//         }
+//   else {
+//     prompt("please type the perfect string ")
+//   }
+// }
+
+
+// let str ="Jawaharlal institute of techology borawan aeiou"
+
+
+// s=0
+
+// for(let v  of str){
+//   if(v=="a" || v=="e" || v=="i"|| v=="o" || v=="u"){
+//     s++;
+//   }
+
+// }
+// console.log(`Number of string in this vowels  ${s}`)
+
+// s=0;
+// function count(str){
+//    for (let v of str){
+//       if(v=="a" || v=="e" || v=="i"|| v=="o" || v=="u"){
+//     s++;
+//   }
+//    }
+// console.log(s)
+// }
+// obj=count("Jawaharlal institute of techology borawan aeiou Rijvan")
+
+
+
+// Palindrom string 
+
+// data="Rijvan"
+// for(let i of  data){
+//   if(i=data){
+//     console.log( )
+//   }
+//   rev=""                                       
+//   i=rev+i
+//   console.log(i)
+
+ 
+// }
+
+
+// console.log(React)
+
+let h1=React.createElement('h1',null,"Hello from Ract")
+
+let perent=document.querySelector("#root")
+let root=ReactDOM.creatRoot(perent)
+root.render(h1)
